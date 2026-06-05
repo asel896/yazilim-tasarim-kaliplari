a07e605ca18c105f3087e46a9e6975530693b83b
Asel <143624281+asel896@users.noreply.github.com>
2026-06-05 11:07:50 +0300

    Update Main.java

--- FactoryMethodDeneme2/src/Main.java
+++ FactoryMethodDeneme2/src/Main.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // oyun motorunu calistiralim
 public class Main {
     public static void main(String[] args) {
@@ -11,21 +11,5 @@ public class Main {
         // Bölüm 2: Gelecekten robotlar geldi
         spawner = new RobotSpawner();
         spawner.spawnAndAttack(); // Çıktı: Robot lazer fırlattı! -40 HP
-=======
-// istemci(client)
-
-// istemci tarafinda kodun icine asla new Truck() veya new Ship() yazmiyoruz. istemci sadece soyut tiplerle konusur
-public class Main {
-    public static void main(String[] args) {
-       Logistics logistics;
-
-       // senaryo a: kara tasimaciligina kara verildi
-        logistics = new RoadLogistics();
-        logistics.planDelivery(); // cikti: kargo karayolu ile tirla tasiniyor
-
-        // senaryo b: deniz tasimaciligina karar verildi
-        logistics = new SeaLogistics();
-        logistics.planDelivery(); // cikti: kargo denizyolu ile gemiyle tasiniyor
->>>>>>> 5c13075986a796efa2013378eb6d2e992660edf6
     }
 }
